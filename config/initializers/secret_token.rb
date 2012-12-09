d30f1459416fb878b47a78510d5befcab8b3dda5
Mark Craig <mlcraig@cisco.com>
2012-12-08 23:59:14 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileAppManager::Application.config.secret_token = 'f5a7480dee53c46f14a2740eaf5541883c30618b5b4d4b8583e66ab1ea1e359e7bab17d1b56cb91ca695ffaf2ebcf00a2a0239c3b7efa516ac93e44af8f5be08'
