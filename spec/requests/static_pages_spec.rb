c8c8639b8ed61e883c81933448b6ca88be42c430
Mark Craig <mlcraig@cisco.com>
2013-01-06 15:49:51 -0800

    Adds a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -16,9 +16,14 @@ describe "StaticPages" do
     	page.should have_selector('h1', :text => 'Mobile App Manager')
     end
 
-    it "should have the right title" do
+    it "should have the base title" do
     	visit '/static_pages/home'
-    	page.should have_selector('title', :text => "#{base_title}Home")
+    	page.should have_selector('title', :text => "Mobile App Manager")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => "| Home")
     end
   end
 
