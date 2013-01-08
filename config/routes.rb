fe569c316c4338cb64c391b22b794769361cc648
Mark Craig <mlcraig@cisco.com>
2013-01-08 12:53:20 -0800

    Finishes the layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 MobileAppManager::Application.routes.draw do
 
+  get "users/new"
+
   # set the root route (creates the root_path and root_url named routes)
   root to: 'static_pages#home'
 
@@ -7,6 +9,7 @@ MobileAppManager::Application.routes.draw do
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  match '/signup',  to: 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,81 +2,35 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  let(:base_title) { "Mobile App Manager | " }
+  subject { page }
 
   describe "Home Page" do
-    it "should fetch home page with 200 ok response" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get root_path
-      response.status.should be(200)
-    end
+    before { visit root_path }
 
-    it "should have content 'Mobile App Manager'" do
-    	visit root_path
-    	page.should have_selector('h1', :text => 'Mobile App Manager')
-    end
-
-    it "should have the base title" do
-    	visit root_path
-    	page.should have_selector('title', :text => "Mobile App Manager")
-    end
-
-    it "should not have a custom page title" do
-      visit root_path
-      page.should_not have_selector('title', :text => "| Home")
-    end
+    it { should have_selector('h1', text: full_title('')) }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector('title', text: "| Home") }
   end
 
 	
   describe "Help Page" do
-    it "should fetch help page with 200 ok response" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get help_path
-      response.status.should be(200)
-    end
+    before { visit help_path }
 
-    it "should have content 'Help'" do
-    	visit help_path
-    	page.should have_selector('h1', :text => 'Help')
-    end
-
-    it "should have the right title" do
-    	visit help_path
-    	page.should have_selector('title', :text => "#{base_title}Help")
-    end
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
   end
 
   describe "About Page" do
-  	 it "should fetch about page with 200 ok response" do
-  	 	get about_path
-  	 	response.status.should be(200)
-  	 end
+    before { visit about_path }
 
-  	 it "should have content 'About Us'" do
-  	 	visit about_path
-  	 	page.should have_selector('h1', :text => 'About Us')
-  	 end
-
-    it "should have the right title" do
-    	visit about_path
-    	page.should have_selector('title', :text => "#{base_title}About Us")
-    end
+    it { should have_selector('h1', text: 'About Us') }
+    it { should have_selector('title', text: full_title("About Us")) }
   end
 
   describe "Contact Page" do
-    it "should fetch contact page with 200 ok response" do
-    	get contact_path
-    	response.status.should be(200)
-    end
+    before { visit contact_path }
 
-    it "should have the h1 'Contact'" do
-      visit contact_path
-      page.should have_selector('h1', :text => "Contact")
-    end
-
-    it "should have the title 'Contact'" do
-      visit contact_path
-      page.should have_selector('title', :text => "#{base_title}Contact")
-    end
+    it { should have_selector('h1', text: "Contact") }
+    it { should have_selector('title', text: full_title("Contact")) }
   end
 end
